44f69c1419d171fbb53b3ad49619f6dc4d69cd7b
DEVELOPER\moonh <jt2302@naver.com>
2024-08-04 00:56:10 +0900

    24.08.03 로그인 화면 마크업

---
+++ src/main/java/com/market/user/UserController.java
@@ -0,0 +1,15 @@
+package com.market.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+	
+	@GetMapping("/sign-in-view")
+	public String signInView() {
+		return "user/signIn";
+	}
+}
