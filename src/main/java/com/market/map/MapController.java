1bf50b094e30b50e2955823838287f57db1c19db
DEVELOPER\moonh <jt2302@naver.com>
2024-08-08 00:12:28 +0900

    동네지도 페이지 생성 https://github.com/KimSungJun-01#5

---
+++ src/main/java/com/market/map/MapController.java
@@ -0,0 +1,15 @@
+package com.market.map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/map")
+@Controller
+public class MapController {
+	
+	@GetMapping("/map-view")
+	public String mapView() {
+		return "map/map";
+	}
+}
