31dff40beb29c1f4c88b046edd32c651872c7479
DEVELOPER\moonh <jt2302@naver.com>
2024-08-16 22:29:43 +0900

    동네지도 생성 KimSungJun-01 #20

--- src/main/java/com/market/map/MapController.java
+++ src/main/java/com/market/map/MapController.java
@@ -1,7 +1,6 @@
 package com.market.map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MapController {
 	
 	@GetMapping("/map-view")
-	public String mapView(Model model) {
+	public String mapView() {
 		return "map/map";
 	}
 }
