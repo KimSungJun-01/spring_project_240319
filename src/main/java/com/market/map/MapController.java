3921286215a9465ce4b09a037eba4423561c93e4
DEVELOPER\moonh <jt2302@naver.com>
2024-08-16 17:12:05 +0900

    네이버 지도 구현 중 KimSungJun-01 #20

--- src/main/java/com/market/map/MapController.java
+++ src/main/java/com/market/map/MapController.java
@@ -1,6 +1,7 @@
 package com.market.map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MapController {
 	
 	@GetMapping("/map-view")
-	public String mapView() {
+	public String mapView(Model model) {
 		return "map/map";
 	}
 }
